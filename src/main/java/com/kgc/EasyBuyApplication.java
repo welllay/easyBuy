2222218f96851eba6e38a1980b1f2c403590b3fe
welllay <784016182@qq.com>
2024-03-17 14:20:39 +0800

    易买网后端项目架构搭建0.0.1base版

---
+++ src/main/java/com/kgc/EasyBuyApplication.java
@@ -0,0 +1,13 @@
+package com.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyBuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyBuyApplication.class, args);
+    }
+
+}
